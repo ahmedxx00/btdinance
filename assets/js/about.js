7e70ab2c983a9a92d1d5f5bf64dcfba4e906aba0
prog_student <madoreal951@gmail.com>
2024-11-29 01:05:58 +0200

    sixteenth commit

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,15 +1,30 @@
 $(document).ready(function () {
-    // ==== Get the styles (properties and values) for the root ====
-  
-    const RS = getComputedStyle(document.querySelector(":root"));
-  
-    //---------- nav-item-home bg -------
-    $("#nav-item-about").css({
-      "background-color": RS.getPropertyValue("--MAIN-BLACK"),
-      color: RS.getPropertyValue("--WHITE"),
-    });
-    //-------------------------------
-  
-    
-  
+  // ==== Get the styles (properties and values) for the root ====
+
+  const RS = getComputedStyle(document.querySelector(":root"));
+
+  //---------- nav-item-home bg -------
+  $("#nav-item-about").css({
+    "background-color": RS.getPropertyValue("--MAIN-BLACK"),
+    color: RS.getPropertyValue("--WHITE"),
   });
+  //-------------------------------
+
+  $(".tab-link").click(function (e) {
+    $(".tab-link").each(function (index, element) {
+      $(element).removeClass("active");
+    });
+
+    $(".tab-body").each(function (index, element) {
+      $(element).removeClass("active");
+      $(element).removeClass("active-content");
+    });
+
+    $(this).addClass("active");
+    $($(this).attr("href")).addClass("active");
+
+    setTimeout(() => {
+      $($(this).attr("href")).addClass("active-content");
+    }, 50);
+  });
+});
